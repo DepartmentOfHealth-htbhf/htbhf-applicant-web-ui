d713b417cf529c75627c30888b2c323a62621a42
Matthew Smith <msmith@equalexperts.com>
2019-01-09 16:49:36 +0000

    HTBHF-119 Updated smoke tests to use enter-name

--- src/test/smoke/test-suite.js
+++ src/test/smoke/test-suite.js
@@ -2,32 +2,32 @@
 'use strict'
 
 const { expect } = require('chai')
-const PersonalDetails = require('../common/page/personal-details')
+const EnterName = require('../common/page/enter-name')
 const Overview = require('../common/page/overview')
 const DriverManager = require('../common/driver/driver-manager')
 const { PUBLIC_BASE_URL } = require('./environment')
 
 const TOTAL_TEST_TIMEOUT_MILLIS = 20000
 
-describe('Personal details', function () {
+describe('Enter name', function () {
   this.timeout(TOTAL_TEST_TIMEOUT_MILLIS)
 
   const driverManager = new DriverManager()
-  let personalDetails
+  let enterName
   let overview
   let driver
 
   beforeEach(async () => {
     driver = driverManager.initialise()
     overview = new Overview(driver)
-    personalDetails = new PersonalDetails(driver)
+    enterName = new EnterName(driver)
   })
 
   afterEach(async function () {
     await driverManager.quit()
   })
 
-  it('successfully displays the personal details page', async () => {
+  it('successfully displays the enter name page', async () => {
     await overview.open(PUBLIC_BASE_URL)
 
     const h1ElementText = await overview.getH1Text()
@@ -38,6 +38,6 @@ describe('Personal details', function () {
 
     await overview.clickStartButton()
 
-    await personalDetails.waitForPageLoad()
+    await enterName.waitForPageLoad()
   })
 })
