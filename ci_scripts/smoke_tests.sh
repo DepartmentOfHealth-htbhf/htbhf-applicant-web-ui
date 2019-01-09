7f22d4123a58f28fa587b3acad2ff2be73f20b6c
Mike Smith <mike.smith@sescon.co.uk>
2019-01-09 11:09:24 +0000

    HTBHFB-119 Move https configuration into the smoke test script.

--- ci_scripts/smoke_tests.sh
+++ ci_scripts/smoke_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-APP_HOST=$1
+APP_HOST=https://$1
 export APP_HOST
 echo "Running smoke tests using APP_HOST=${APP_HOST}, APP_NAME=${APP_NAME}"
-echo "test/smoke/environment.js:"
-cat test/smoke/environment.js
 
 npm run test:smoke
--- src/test/smoke/environment.js
+++ src/test/smoke/environment.js
@@ -3,7 +3,7 @@
 
 const { APP_HOST, SCREEN_RESOLUTION } = require('../config')
 
-const PUBLIC_BASE_URL = `https://${APP_HOST}`
+const PUBLIC_BASE_URL = `${APP_HOST}`
 
 module.exports = {
   PUBLIC_BASE_URL,
