f69f78657bf410b7ed7b23735ad0bd98cf808a2e
David Wybourn <david.wybourn@gmail.com>
2019-01-10 14:10:23 +0000

    HTBHF-118 Changed how the application sets cookies to be secure.

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,6 @@
+const { path } = require('ramda')
+
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'production',
+  USE_UNSECURE_COOKIE: path(['env', 'USE_UNSECURE_COOKIE'], process) === 'true',
   CLAIMANT_SERVICE_URL: process.env.CLAIMANT_SERVICE_URL
 }
