22f6095addb4840a4270b2415260feaf98d10b21
Gethyn Jones <gethyn1@gmail.com>
2019-01-22 14:17:53 +0000

    HTBHF-358 Define DOB template and translations

--- src/web/routes/application/enter-dob/enter-dob.js
+++ src/web/routes/application/enter-dob/enter-dob.js
@@ -1,10 +1,21 @@
+const pageContent = ({ translate }) => ({
+  title: translate('enterDob.title'),
+  heading: translate('enterDob.heading'),
+  ninoLabel: translate('enterDob.ninoLabel'),
+  hint: translate('enterDob.hint'),
+  buttonText: translate('buttons:continue'),
+  dayLabel: translate('enterDob.dayLabel'),
+  monthLabel: translate('enterDob.monthLabel'),
+  yearLabel: translate('enterDob.yearLabel')
+})
+
 const enterDob = {
   path: '/enter-dob',
   next: '/check',
   template: 'enter-dob',
   sanitize: [],
   validate: [],
-  pageContent: () => {}
+  pageContent
 }
 
 module.exports = {
