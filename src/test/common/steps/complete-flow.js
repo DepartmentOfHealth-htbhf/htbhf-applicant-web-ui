88032c4d45ab882eb3d8869a70543af698a8588a
David Wybourn <david.wybourn@gmail.com>
2019-01-24 15:19:06 +0000

    NOJIRA Refactored common test code.

--- src/test/common/steps/complete-flow.js
+++ src/test/common/steps/complete-flow.js
@@ -1,7 +1,6 @@
 const { When } = require('cucumber')
-const { assert } = require('chai')
 
-const pages = require('./pages')
+const { enterNameAndSubmit, enterNinoAndSubmit, enterDateOfBirth } = require('./common-steps')
 
 const VALID_NINO = 'QQ123456C'
 
@@ -10,34 +9,3 @@ When(/^I enter a valid set of details$/, async function () {
   await enterNinoAndSubmit(VALID_NINO)
   await enterDateOfBirth('30', '12', '1980')
 })
-
-// TODO - Need to refactor these into a common file to remove duplication with other step files.
-async function enterNameAndSubmit (firstName, lastName) {
-  try {
-    await pages.enterName.enterFirstName(firstName)
-    await pages.enterName.enterLastName(lastName)
-    await pages.enterName.submitForm()
-  } catch (error) {
-    assert.fail(`Unexpected error caught trying to enter the name and submit the page - ${error}`)
-  }
-}
-
-async function enterNinoAndSubmit (nino) {
-  try {
-    await pages.enterNino.enterNino(nino)
-    await pages.enterNino.submitForm()
-  } catch (error) {
-    assert.fail(`Unexpected error caught trying to enter the national insurance number and submit the page - ${error}`)
-  }
-}
-
-async function enterDateOfBirth (day, month, year) {
-  try {
-    await pages.enterDOB.enterDay(day)
-    await pages.enterDOB.enterMonth(month)
-    await pages.enterDOB.enterYear(year)
-    await pages.enterDOB.submitForm()
-  } catch (error) {
-    assert.fail(`Unexpected error caught trying to enter the date of birth and submit the page - ${error}`)
-  }
-}
--- src/test/common/steps/enter-dob-steps.js
+++ src/test/common/steps/enter-dob-steps.js
@@ -2,6 +2,7 @@ const { Given, When, Then } = require('cucumber')
 const { expect, assert } = require('chai')
 
 const pages = require('./pages')
+const { enterDateOfBirth, assertErrorHeaderTextPresent } = require('./common-steps')
 
 Given(/^I am on the enter date of birth page$/, async function () {
   await pages.enterDOB.open(pages.url)
@@ -16,35 +17,14 @@ Then(/^the check your details page is shown$/, async function () {
 })
 
 Then(/^I am informed that a valid date of birth is required$/, async function () {
-  await assertErrorHeaderTextPresent()
+  await assertErrorHeaderTextPresent(pages.enterDOB)
   await assertDateOfBirthErrorPresent('Enter a valid date of birth')
 })
 
 Then(/^I am informed that my date of birth should be in the past$/, async function () {
-  await assertErrorHeaderTextPresent()
+  await assertErrorHeaderTextPresent(pages.enterDOB)
   await assertDateOfBirthErrorPresent('Date of birth must be in the past')
 })
-
-async function enterDateOfBirth (day, month, year) {
-  try {
-    await pages.enterDOB.enterDay(day)
-    await pages.enterDOB.enterMonth(month)
-    await pages.enterDOB.enterYear(year)
-    await pages.enterDOB.submitForm()
-  } catch (error) {
-    assert.fail(`Unexpected error caught trying to enter the date of birth and submit the page - ${error}`)
-  }
-}
-
-async function assertErrorHeaderTextPresent () {
-  try {
-    await pages.enterDOB.waitForPageLoad()
-    const errorHeader = await pages.enterDOB.getPageErrorHeaderText()
-    expect(errorHeader).to.equal('There is a problem')
-  } catch (error) {
-    assert.fail(`Unexpected error caught trying to assert error header text is present - ${error}`)
-  }
-}
 
 async function assertDateOfBirthErrorPresent (expectedErrorMessage) {
   try {
