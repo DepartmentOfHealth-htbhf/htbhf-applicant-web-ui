85fc771b22c2c95ed1b16b1b454367be44caa30b
Matthew Smith <msmith@equalexperts.com>
2019-01-26 20:51:32 +0000

    NO-JIRA Replaced Gruntfile with npm-run-all and separated the json output with browser metadata

--- src/test/compatibility/compatibility-report.js
+++ src/test/compatibility/compatibility-report.js
@@ -2,9 +2,10 @@ const reporter = require('cucumber-html-reporter')
 
 const options = {
   theme: 'bootstrap',
-  jsonFile: 'build/reports/compatibility-report.json',
+  jsonDir: 'build/reports/compatibility',
   output: 'build/reports/compatibility-report.html',
   reportSuiteAsScenarios: true,
+  brandTitle: 'Compatibility Tests',
   launchReport: false
 }
 
