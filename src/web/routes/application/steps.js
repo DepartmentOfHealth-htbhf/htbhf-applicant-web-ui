593a448d46978aec503b12532bfaca18e40291b4
David Wybourn <david.wybourn@gmail.com>
2019-01-28 14:34:22 +0000

    HTBHF-427 Added address route and template.

--- src/web/routes/application/steps.js
+++ src/web/routes/application/steps.js
@@ -2,12 +2,14 @@ const { enterNino } = require('./enter-nino')
 const { enterName } = require('./enter-name')
 const { enterDob } = require('./enter-dob')
 const { areYouPregnant } = require('./are-you-pregnant')
+const { cardAddress } = require('./card-address')
 
 const steps = [
   enterNino,
   enterName,
   enterDob,
-  areYouPregnant
+  areYouPregnant,
+  cardAddress
 ]
 
 module.exports = { steps }
