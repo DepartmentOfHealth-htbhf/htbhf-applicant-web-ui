1da5eb582ce70bebcdda649f73849a81b5b1b102
Gethyn Jones <gethyn1@gmail.com>
2019-01-30 11:43:27 +0000

    HTBHF-413 add validation composition tests

--- src/web/routes/application/are-you-pregnant/validate.test.js
+++ src/web/routes/application/are-you-pregnant/validate.test.js
@@ -1,7 +1,71 @@
 const test = require('tape')
-const { addExpectedDeliveryDateToBody, validateExpectedDeliveryDate } = require('./validate')
+const { applyExpressValidation } = require('../common/test/apply-express-validation')
+const { validate, addExpectedDeliveryDateToBody, validateExpectedDeliveryDate } = require('./validate')
 const { dateAsString } = require('../common/formatters')
 const { YES, NO } = require('./constants')
+
+const res = {}
+const next = () => {}
+
+test('validation middleware passes with valid body', async (t) => {
+  const sixMonthsInFuture = dateAsString({ monthAdjustment: 6 }).split('-')
+
+  const req = {
+    t: string => string,
+    body: {
+      areYouPregnant: YES,
+      'expectedDeliveryDate-day': sixMonthsInFuture[2],
+      'expectedDeliveryDate-month': sixMonthsInFuture[1],
+      'expectedDeliveryDate-year': sixMonthsInFuture[0]
+    }
+  }
+
+  const result = await applyExpressValidation(req, validate)
+  t.equal(result.isEmpty(), true)
+  t.end()
+})
+
+test('validation middleware errors for are you pregnant field', async (t) => {
+  const sixMonthsInFuture = dateAsString({ monthAdjustment: 6 }).split('-')
+
+  const req = {
+    t: string => string,
+    body: {
+      areYouPregnant: 'maybe',
+      'expectedDeliveryDate-day': sixMonthsInFuture[2],
+      'expectedDeliveryDate-month': sixMonthsInFuture[1],
+      'expectedDeliveryDate-year': sixMonthsInFuture[0]
+    }
+  }
+
+  const result = await applyExpressValidation(req, validate)
+  const error = result.array()[0]
+  t.equal(result.isEmpty(), false, 'should have errors')
+  t.equal(error.param, 'areYouPregnant', 'error should be associated with correct field')
+  t.equal(error.msg, 'validation:selectYesOrNo', 'error should have correct message')
+  t.end()
+})
+
+test('validation middleware errors for are you pregnant field', async (t) => {
+  const twelveMonthsInFuture = dateAsString({ monthAdjustment: 12 }).split('-')
+
+  const req = {
+    t: string => string,
+    body: {
+      areYouPregnant: YES,
+      'expectedDeliveryDate-day': twelveMonthsInFuture[2],
+      'expectedDeliveryDate-month': twelveMonthsInFuture[1],
+      'expectedDeliveryDate-year': twelveMonthsInFuture[0]
+    }
+  }
+
+  const result = await applyExpressValidation(req, validate)
+  const error = result.array()[0]
+  t.equal(result.isEmpty(), false, 'should have errors')
+  t.equal(error.param, 'expectedDeliveryDate', 'error should be associated with correct field')
+  t.equal(error.msg, 'validation:expectedDeliveryDateInvalidTooFarInFuture', 'error should have correct message')
+  t.end()
+})
 
 test('validateExpectedDeliveryDate()', (t) => {
   const req = {
@@ -38,8 +102,6 @@ test('validateExpectedDeliveryDateNotPregnant()', (t) => {
 
 test('addExpectedDeliveryDateToBody() should not add key to body if areYouPregnant is undefined', (t) => {
   const req = { body: {} }
-  const res = {}
-  const next = () => {}
 
   addExpectedDeliveryDateToBody(req, res, next)
   t.equal(typeof req.body.expectedDeliveryDate, 'undefined', 'should not add key to body if areYouPregnant is undefined')
@@ -52,8 +114,6 @@ test(`addExpectedDeliveryDateToBody() should not add key to body if areYouPregna
       areYouPregnant: NO
     }
   }
-  const res = {}
-  const next = () => {}
 
   addExpectedDeliveryDateToBody(req, res, next)
   t.equal(typeof req.body.expectedDeliveryDate, 'undefined', `should not add key to body if areYouPregnant is ${NO}`)
@@ -69,8 +129,6 @@ test(`addExpectedDeliveryDateToBody() should add key to body if areYouPregnant i
       'expectedDeliveryDate-year': '2222'
     }
   }
-  const res = {}
-  const next = () => {}
 
   addExpectedDeliveryDateToBody(req, res, next)
   t.equal(req.body.expectedDeliveryDate, '2222-02-02', `should add key to body if areYouPregnant is ${YES}`)
