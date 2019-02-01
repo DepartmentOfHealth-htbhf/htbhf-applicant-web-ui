22476be7f3ccbf7baba3113175401d1cbd1362cb
Mike Smith <mike.smith@sescon.co.uk>
2019-02-01 15:16:17 +0000

    HTBHF-454 Add the ability to hide the expected delivery date if the claimant hasn't declared they are pregnant.

--- src/web/routes/application/check/get.js
+++ src/web/routes/application/check/get.js
@@ -1,4 +1,6 @@
-const pageContent = ({ translate }) => ({
+const { YES } = require('../are-you-pregnant/constants')
+
+const pageContent = ({ translate, claim }) => ({
   title: translate('check.title'),
   heading: translate('check.heading'),
   name: translate('check.name'),
@@ -9,13 +11,28 @@ const pageContent = ({ translate }) => ({
   address: translate('check.address'),
   sendApplicationHeader: translate('check.sendApplicationHeader'),
   sendApplicationText: translate('check.sendApplicationText'),
-  buttonText: translate('check.buttonText')
+  buttonText: translate('check.buttonText'),
+  expectedDeliveryDateRow: buildExpectedDeliveryDateRow(translate, claim)
 })
+
+// We need to build up this row here because we cannot dynamically hide a using the govukTable component based on session data.
+const buildExpectedDeliveryDateRow = (translate, claim) => {
+  if (claim.areYouPregnant === YES) {
+    return [
+      {
+        text: translate('check.dueDate')
+      },
+      {
+        text: [claim['expectedDeliveryDate-day'], claim['expectedDeliveryDate-month'], claim['expectedDeliveryDate-year']].join(' ')
+      }
+    ]
+  }
+}
 
 const getCheck = (req, res) => {
   res.render('check', {
-    ...pageContent({ translate: req.t }),
     claim: req.session.claim,
+    ...pageContent({ translate: req.t, claim: req.session.claim }),
     csrfToken: req.csrfToken()
   })
 }
