4037324cda50cf8ed7c5fa359cb869b244afa920
Mike Smith <mike.smith@sescon.co.uk>
2019-02-04 11:55:40 +0000

    HTBHF-454 Refactored out the check page row data out of the Nunjucks so that we can dynamically build up the contents, mainly due to the presence of optional data.

--- src/web/routes/application/check/get.js
+++ src/web/routes/application/check/get.js
@@ -3,30 +3,83 @@ const { YES } = require('../are-you-pregnant/constants')
 const pageContent = ({ translate, claim }) => ({
   title: translate('check.title'),
   heading: translate('check.heading'),
-  name: translate('check.name'),
-  nino: translate('check.nationalInsuranceNumber'),
-  dateOfBirth: translate('check.dateOfBirth'),
-  areYouPregnant: translate('check.areYouPregnant'),
-  dueDate: translate('check.dueDate'),
-  address: translate('check.address'),
   sendApplicationHeader: translate('check.sendApplicationHeader'),
   sendApplicationText: translate('check.sendApplicationText'),
   buttonText: translate('buttons:acceptAndSend'),
-  expectedDeliveryDateRow: buildExpectedDeliveryDateRow(translate, claim)
+  checkRowData: buildCheckRowData(translate, claim)
 })
 
-// We need to build up this row here because we cannot dynamically hide a using the govukTable component based on session data.
+const buildCheckRowData = (translate, claim) => {
+  return [
+    buildNameRow(translate, claim),
+    buildNinoRow(translate, claim),
+    buildDateOfBirthRow(translate, claim),
+    buildAreYouPregnantRow(translate, claim),
+    buildExpectedDeliveryDateRow(translate, claim),
+    buildAddressRow(translate, claim)
+  ].filter(row => row !== undefined)
+}
+
+const buildNameRow = (translate, claim) => {
+  return buildRowData(
+    translate('check.name'),
+    [claim.firstName, claim.lastName].join(' ')
+  )
+}
+
+const buildNinoRow = (translate, claim) => {
+  return buildRowData(
+    translate('check.nationalInsuranceNumber'),
+    claim.nino
+  )
+}
+
+const buildDateOfBirthRow = (translate, claim) => {
+  return buildRowData(
+    translate('check.dateOfBirth'),
+    [claim['dateOfBirth-day'], claim['dateOfBirth-month'], claim['dateOfBirth-year']].join(' ')
+  )
+}
+
+const buildAreYouPregnantRow = (translate, claim) => {
+  return buildRowData(
+    translate('check.areYouPregnant'),
+    claim.areYouPregnant
+  )
+}
+
 const buildExpectedDeliveryDateRow = (translate, claim) => {
   if (claim.areYouPregnant === YES) {
-    return [
-      {
-        text: translate('check.dueDate')
-      },
-      {
-        text: [claim['expectedDeliveryDate-day'], claim['expectedDeliveryDate-month'], claim['expectedDeliveryDate-year']].join(' ')
-      }
-    ]
+    return buildRowData(
+      translate('check.dueDate'),
+      [claim['expectedDeliveryDate-day'], claim['expectedDeliveryDate-month'], claim['expectedDeliveryDate-year']].join(' ')
+    )
   }
+}
+
+// We need to build up this row here because we cannot dynamically hide a using the govukTable component based on session data.
+const buildAddressRow = (translate, claim) => {
+  return buildRowData(
+    translate('check.address'),
+    [
+      claim.addressLine1,
+      claim.addressLine2,
+      claim.townOrCity,
+      claim.postcode
+    ].filter(item => (item !== undefined && item.length !== 0))
+      .join('<br/>')
+  )
+}
+
+const buildRowData = (heading, content) => {
+  return [
+    {
+      text: heading
+    },
+    {
+      text: content
+    }
+  ]
 }
 
 const getCheck = (req, res) => {
