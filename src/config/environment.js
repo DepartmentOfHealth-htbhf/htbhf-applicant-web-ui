4a5bb7f03146c15885ce50520c8d414712570496
David Wybourn <dwybourn@users.noreply.github.com>
2019-02-05 16:23:50 +0000

    HTBHF-112 Added google analytics script. (#121)
    * HTBHF-112 Added ability to read environment variables from bounded service.

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,12 @@
 const { path } = require('ramda')
+const { getVariableServiceCredentials } = require('./vcap-services')
+
+const gaTrackingId = process.env.VCAP_SERVICES
+  ? getVariableServiceCredentials().GA_TRACKING_ID
+  : process.env.GA_TRACKING_ID || ''
 
 module.exports = {
   USE_UNSECURE_COOKIE: path(['env', 'USE_UNSECURE_COOKIE'], process) === 'true',
-  CLAIMANT_SERVICE_URL: process.env.CLAIMANT_SERVICE_URL
+  CLAIMANT_SERVICE_URL: process.env.CLAIMANT_SERVICE_URL,
+  GA_TRACKING_ID: gaTrackingId
 }
---
+++ src/config/vcap-services.js
@@ -0,0 +1,38 @@
+const { path } = require('ramda')
+const cfenv = require('cfenv')
+
+function getVCAPServices (service) {
+  const services = path(['services', service], cfenv.getAppEnv())
+  if (!Array.isArray(services)) {
+    throw new Error(`Expected an array of ${service} services, got ${typeof services}`)
+  }
+  return services
+}
+
+function getVariableService (userProvidedServices) {
+  const variableServices = userProvidedServices.filter(service => service.instance_name === 'variable-service')
+  if (variableServices.length !== 1) {
+    throw new Error(`Expected exactly one variable-service, instead found ${variableServices.length}`)
+  }
+  return variableServices[0]
+}
+
+const getVariableServiceCredentials = () => {
+  const userProvidedServices = getVCAPServices('user-provided')
+  const variableService = getVariableService(userProvidedServices)
+  return variableService.credentials
+}
+
+const getRedisCredentials = () => {
+  const redisServices = getVCAPServices('redis')
+  if (redisServices.length !== 1) {
+    throw new Error(`Expected exactly one redis service, instead found ${redisServices.length}`)
+  }
+
+  return redisServices[0].credentials
+}
+
+module.exports = {
+  getVariableServiceCredentials,
+  getRedisCredentials
+}
