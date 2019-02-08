463a54ed7160e6daa95ced026118ddf377f2ae99
Matt <msmith@equalexperts.com>
2019-02-08 08:40:03 +0000

    HTBHF-489 Added another unit test for state-machine

--- src/web/routes/application/common/state-machine/state-machine.test.js
+++ src/web/routes/application/common/state-machine/state-machine.test.js
@@ -9,6 +9,13 @@ test('dispatch() should redirect to given page when no state defined in session'
   t.end()
 })
 
+test('dispatch() should redirect to given page when state of IN_PROGRESS defined in session', async (t) => {
+  const req = { method: 'POST', session: { state: states.IN_PROGRESS } }
+
+  t.equal(stateMachine.dispatch(actions.GET_NEXT_PAGE, req, '/mynextpage'), '/mynextpage')
+  t.end()
+})
+
 test('dispatch() should redirect to check page when state of IN_REVIEW defined in session', async (t) => {
   const req = { method: 'POST', session: { state: states.IN_REVIEW } }
 
