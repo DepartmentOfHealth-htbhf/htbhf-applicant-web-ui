ebdae0581ca8929a24a511e7d0b8790f57816b72
Gethyn Jones <gethyn1@gmail.com>
2019-02-12 14:53:00 +0000

    HTBHF-540 create winston logger

--- src/web/server/server.js
+++ src/web/server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const { logger } = require('./logger')
 const { configureSecurity } = require('./configure-security')
 const { registerRoutes } = require('../routes')
 const { initialiseSession } = require('./session')
@@ -21,10 +22,10 @@ const configureStaticPaths = (app) => {
   app.use('/assets', express.static(path.resolve('src/web/assets'))) /* 3 */
 }
 
-const listen = (config, app) =>
-  app.listen(config.server.PORT, () => console.log(
-    `App listening on port ${config.server.PORT}`)
-  )
+const listen = (config, app) => logger.log({
+  level: 'info',
+  message: `App listening on port ${config.server.PORT}`
+})
 
 const start = (config, app) => () => {
   app.use(compression())
