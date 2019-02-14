09ef6a000b470a63dac862f242ba5ac1b0cba057
Gethyn Jones <gethyn1@gmail.com>
2019-02-14 14:26:29 +0000

    HTBHF-537 add session ID and request ID to logs

---
+++ src/web/server/logger/format.test.js
@@ -0,0 +1,33 @@
+const test = require('tape')
+const { logFormatter } = require('./format')
+
+test('logFormatter() formats log correctly if IDs are not available', (t) => {
+  const req = {}
+  const level = 'info'
+  const message = 'log message'
+  const timestamp = '2019-02-14'
+
+  const result = logFormatter(req, { level, message, timestamp })
+  const expected = '2019-02-14 INFO [][] log message'
+
+  t.equal(result, expected, 'formats log correctly if IDs are not available')
+  t.end()
+})
+
+test('logFormatter() formats log correctly if IDs are available', (t) => {
+  const req = {
+    headers: {
+      'X-Request-ID': '1234'
+    },
+    sessionID: '5678'
+  }
+  const level = 'info'
+  const message = 'log message'
+  const timestamp = '2019-02-14'
+
+  const result = logFormatter(req, { level, message, timestamp })
+  const expected = '2019-02-14 INFO [5678][1234] log message'
+
+  t.equal(result, expected, 'formats log correctly if IDs are available')
+  t.end()
+})
