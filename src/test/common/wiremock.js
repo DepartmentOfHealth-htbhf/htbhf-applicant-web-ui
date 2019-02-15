34c8bcb8f26d6b7cc1e377143da908f70d5a7fd1
Mike Smith <mike.smith@sescon.co.uk>
2019-02-15 09:59:59 +0000

    HTBHF-534 Just JSON stringify rather than a template string.

--- src/test/common/wiremock.js
+++ src/test/common/wiremock.js
@@ -2,15 +2,15 @@ const { postJsonData, performDelete } = require('./request')
 
 const WIREMOCK_MAPPING_URL = process.env.WIREMOCK_URL ? `${process.env.WIREMOCK_URL}/__admin/mappings` : 'http://localhost:8090/__admin/mappings'
 
-const SUCCESSFUL_CLAIMS_MAPPING = `{
-    "request": {
-      "method": "POST",
-      "url": "/v1/claims"
-    },
-    "response": {
-      "status": 201
-    }
-  }`
+const SUCCESSFUL_CLAIMS_MAPPING = JSON.stringify({
+  'request': {
+    'method': 'POST',
+    'url': '/v1/claims'
+  },
+  'response': {
+    'status': 201
+  }
+})
 
 async function setupSuccessfulWiremockClaimMapping () {
   await postJsonData(WIREMOCK_MAPPING_URL, SUCCESSFUL_CLAIMS_MAPPING)
