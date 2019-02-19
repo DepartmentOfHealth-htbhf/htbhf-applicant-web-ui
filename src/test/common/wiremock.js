0024a023180262beeedd0fb06747010c3ea7e215
Gethyn Jones <gethyn1@gmail.com>
2019-02-19 10:05:16 +0000

    HTBHF-566 add headers to wiremock claimant service mapping

--- src/test/common/wiremock.js
+++ src/test/common/wiremock.js
@@ -1,11 +1,23 @@
 const { postJsonData, performDelete } = require('./request')
 
-const WIREMOCK_MAPPING_URL = process.env.WIREMOCK_URL ? `${process.env.WIREMOCK_URL}/__admin/mappings` : 'http://localhost:8090/__admin/mappings'
+const WIREMOCK_MAPPING_URL = process.env.WIREMOCK_URL
+  ? `${process.env.WIREMOCK_URL}/__admin/mappings`
+  : 'http://localhost:8090/__admin/mappings'
+
+const ID_HEADERS_MATCH = '([A-Za-z0-9_-])+'
 
 const SUCCESSFUL_CLAIMS_MAPPING = JSON.stringify({
   'request': {
     'method': 'POST',
-    'url': '/v1/claims'
+    'url': '/v1/claims',
+    'headers': {
+      'X-Request-ID': {
+        'matches': ID_HEADERS_MATCH
+      },
+      'X-Session-ID': {
+        'matches': ID_HEADERS_MATCH
+      }
+    }
   },
   'response': {
     'status': 201
