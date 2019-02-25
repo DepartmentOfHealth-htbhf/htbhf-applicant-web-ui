382585e3a7f8de080686b723d47ee69b4fda42e4
Matt Smith <msmith@equalexperts.com>
2019-02-25 14:18:07 +0000

    HTBHF-272 Use x-vcap-request-id header if available

--- src/web/server/headers/request-id.js
+++ src/web/server/headers/request-id.js
@@ -1,11 +1,14 @@
+const REQUEST_ID_HEADER = 'x-request-id'
+const X_VCAP_REQUEST_ID = 'x-vcap-request-id'
 const requestID = (uuid) => (req, res, next) => {
-  if (!req.headers['X-Request-ID']) {
-    req.headers['X-Request-ID'] = uuid()
+  if (!req.headers[REQUEST_ID_HEADER]) {
+    req.headers[REQUEST_ID_HEADER] = req.headers[X_VCAP_REQUEST_ID] || uuid()
   }
 
   next()
 }
 
 module.exports = {
+  REQUEST_ID_HEADER,
   requestID
 }
