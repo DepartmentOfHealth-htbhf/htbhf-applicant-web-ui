3786ed533b660e9585bf186d2f5687f331dd5bd0
Gethyn Jones <gethyn1@gmail.com>
2019-03-01 16:19:49 +0000

    HTBHF-606 set next allowed step if none exists in session

--- src/web/routes/application/middleware/handle-path-request.js
+++ src/web/routes/application/middleware/handle-path-request.js
@@ -1,7 +1,18 @@
-const handlePathRequest = (steps) => (req, res, next) => {
+const { CHECK_URL } = require('../common/constants')
+
+const getPathsInSequence = (steps) => [...steps.map(step => step.path), CHECK_URL]
+
+const middleware = (pathsInSequence) => (req, res, next) => {
+  if (!req.session.nextAllowedStep) {
+    req.session.nextAllowedStep = pathsInSequence[0]
+  }
+
   next()
 }
 
+const handlePathRequest = (steps) => middleware(getPathsInSequence(steps))
+
 module.exports = {
+  getPathsInSequence,
   handlePathRequest
 }
