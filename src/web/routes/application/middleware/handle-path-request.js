97727ec48eaa932ae71be1213928c67142af120c
Gethyn Jones <gethyn1@gmail.com>
2019-03-01 16:38:58 +0000

    HTBHF-607 redirect if requested apply journey path is not allowed

--- src/web/routes/application/middleware/handle-path-request.js
+++ src/web/routes/application/middleware/handle-path-request.js
@@ -1,10 +1,18 @@
+const { equals } = require('ramda')
 const { CHECK_URL } = require('../common/constants')
 
 const getPathsInSequence = (steps) => [...steps.map(step => step.path), CHECK_URL]
 
+const isPathAllowed = (sequence, allowed, path) =>
+  sequence.findIndex(equals(path)) <= sequence.findIndex(equals(allowed))
+
 const middleware = (pathsInSequence) => (req, res, next) => {
   if (!req.session.nextAllowedStep) {
     req.session.nextAllowedStep = pathsInSequence[0]
+  }
+
+  if (!isPathAllowed(pathsInSequence, req.session.nextAllowedStep, req.path)) {
+    return res.redirect(req.session.nextAllowedStep)
   }
 
   next()
@@ -14,5 +22,6 @@ const handlePathRequest = (steps) => middleware(getPathsInSequence(steps))
 
 module.exports = {
   getPathsInSequence,
+  isPathAllowed,
   handlePathRequest
 }
--- src/web/routes/application/middleware/handle-path-request.test.js
+++ src/web/routes/application/middleware/handle-path-request.test.js
@@ -1,15 +1,34 @@
 const test = require('tape')
 const sinon = require('sinon')
 const { CHECK_URL } = require('../common/constants')
-const { getPathsInSequence, handlePathRequest } = require('./handle-path-request')
+const { getPathsInSequence, isPathAllowed, handlePathRequest } = require('./handle-path-request')
 
 const steps = [{ path: '/first', next: '/second' }, { path: '/second' }]
+const paths = ['/first', '/second', '/third', '/fourth']
 
 test('getPathsInSequence() returns the correct sequence of paths', (t) => {
   const expected = ['/first', '/second', CHECK_URL]
   const result = getPathsInSequence(steps)
 
   t.deepEqual(result, expected, 'returns the correct sequence of paths')
+  t.end()
+})
+
+test('isPathAllowed() should return true if path is before allowed in sequence', (t) => {
+  const result = isPathAllowed(paths, '/third', '/second')
+  t.equal(result, true, 'returns true if path is before allowed in sequence')
+  t.end()
+})
+
+test('isPathAllowed() should return true if path matches allowed', (t) => {
+  const result = isPathAllowed(paths, '/third', '/third')
+  t.equal(result, true, 'returns true if path matches allowed')
+  t.end()
+})
+
+test('isPathAllowed() should return false if path is after allowed in sequence', (t) => {
+  const result = isPathAllowed(paths, '/third', '/fourth')
+  t.equal(result, false, 'returns false if path is after allowed in sequence')
   t.end()
 })
 
@@ -23,7 +42,45 @@ test('handlePathRequest() should set next allowed path to first in sequence if n
 
   handlePathRequest(steps)(req, res, next)
 
-  t.equal(req.session.nextAllowedStep, '/first', 'should set next allowed path to first in sequence')
+  t.equal(req.session.nextAllowedStep, '/first', 'it should set next allowed path to first in sequence')
+  t.equal(next.called, true, 'it should call next()')
+  t.end()
+})
+
+test('handlePathRequest() should redirect to next allowed step if requested path is not allowed', (t) => {
+  const req = {
+    path: '/second',
+    session: {
+      nextAllowedStep: '/first'
+    }
+  }
+
+  const redirect = sinon.spy()
+  const res = { redirect }
+  const next = sinon.spy()
+
+  handlePathRequest(steps)(req, res, next)
+
+  t.equal(redirect.calledWith('/first'), true, 'it should call redirect() with next allowed step')
+  t.equal(next.called, false, 'it should not call next()')
+  t.end()
+})
+
+test('handlePathRequest() should call next() if requested path is allowed', (t) => {
+  const req = {
+    path: '/second',
+    session: {
+      nextAllowedStep: '/second'
+    }
+  }
+
+  const redirect = sinon.spy()
+  const res = { redirect }
+  const next = sinon.spy()
+
+  handlePathRequest(steps)(req, res, next)
+
+  t.equal(redirect.called, false, 'it should not call redirect()')
   t.equal(next.called, true, 'it should call next()')
   t.end()
 })
