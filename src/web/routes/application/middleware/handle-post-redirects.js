06e0ac5d7a385596749cb5b58ad26ad46f2e7ffd
Gethyn Jones <gethyn1@gmail.com>
2019-03-01 14:35:27 +0000

    HTBHF-605 invert error checking for redirect

--- src/web/routes/application/middleware/handle-post-redirects.js
+++ src/web/routes/application/middleware/handle-post-redirects.js
@@ -1,12 +1,12 @@
 const { stateMachine, actions } = require('../common/state-machine')
 
 const handlePostRedirects = (steps) => (req, res, next) => {
-  if (!res.locals.errors) {
-    const nextPage = stateMachine.dispatch(actions.GET_NEXT_PATH, req, steps, req.path)
-    return res.redirect(nextPage)
+  if (res.locals.errors) {
+    return next()
   }
 
-  next()
+  const nextPage = stateMachine.dispatch(actions.GET_NEXT_PATH, req, steps, req.path)
+  return res.redirect(nextPage)
 }
 
 module.exports = {
