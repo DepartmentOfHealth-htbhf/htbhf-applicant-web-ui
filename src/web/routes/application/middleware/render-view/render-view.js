8af30853a069547ae28eb2f434dffaefcb59eefb
Gethyn Jones <gethyn1@gmail.com>
2019-03-01 14:21:40 +0000

    HTBHF-607 remove redirects from render view

--- src/web/routes/application/middleware/render-view/render-view.js
+++ src/web/routes/application/middleware/render-view/render-view.js
@@ -1,11 +1,4 @@
-const { stateMachine, actions } = require('../../common/state-machine')
-
-const renderView = (steps, step) => (req, res) => {
-  if (req.method === 'POST' && !res.locals.errors) {
-    const nextPage = stateMachine.dispatch(actions.GET_NEXT_PATH, req, steps, req.path)
-    return res.redirect(nextPage)
-  }
-
+const renderView = (step) => (req, res) => {
   res.render(step.template, {
     ...step.pageContent({ translate: req.t }),
     csrfToken: req.csrfToken()
