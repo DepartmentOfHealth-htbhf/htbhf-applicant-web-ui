e2f20faaf93ca231fb05e578f51a5dd75919af1d
Gethyn Jones <gethyn1@gmail.com>
2019-03-04 11:49:04 +0000

    HTBHF-629 add path request middleware to check details route

--- src/web/routes/application/check/check.js
+++ src/web/routes/application/check/check.js
@@ -1,12 +1,13 @@
 const { getCheck } = require('./get')
 const { postCheck } = require('./post')
 const { CHECK_URL } = require('../common/constants')
+const { handleRequestForPath } = require('../middleware')
 
-const registerCheckRoutes = (csrfProtection, config, app) => {
+const registerCheckRoutes = (csrfProtection, steps, config, app) => {
   app
     .route(CHECK_URL)
-    .get(csrfProtection, getCheck)
-    .post(csrfProtection, postCheck(config))
+    .get(csrfProtection, handleRequestForPath(steps), getCheck)
+    .post(csrfProtection, handleRequestForPath(steps), postCheck(config))
 }
 
 module.exports = {
