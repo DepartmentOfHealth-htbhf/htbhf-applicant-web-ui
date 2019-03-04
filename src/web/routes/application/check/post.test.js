eac834a9dfd3a42476d713540328e17221f490e7
Gethyn Jones <gethyn1@gmail.com>
2019-03-04 15:05:31 +0000

    HTBHF-629 add unit tests for successful POST to /check

--- src/web/routes/application/check/post.test.js
+++ src/web/routes/application/check/post.test.js
@@ -1,10 +1,16 @@
 const test = require('tape')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
+const { states } = require('../common/state-machine')
+const { CHECK_URL, CONFIRM_URL } = require('../common/constants')
 
 const post = sinon.stub()
+
 const { postCheck } = proxyquire('./post', {
-  'request-promise': { post }
+  'request-promise': { post },
+  './create-request-body': {
+    createRequestBody: () => {}
+  }
 })
 
 const config = {
@@ -23,6 +29,33 @@ test('unsuccessful post calls next with error', async (t) => {
   try {
     await postCheck(config)(req, res, next)
     t.equal(next.calledWith(sinon.match.instanceOf(Error)), true)
+    t.end()
+  } catch (error) {
+    t.fail(error)
+  }
+})
+
+test(`successful post sets next allowed step to ${CONFIRM_URL}`, async (t) => {
+  const steps = []
+  const next = sinon.spy()
+  const redirect = sinon.spy()
+  const req = {
+    path: CHECK_URL,
+    headers: [],
+    sessionID: '123',
+    claim: {},
+    session: {
+      state: states.IN_REVIEW
+    }
+  }
+  const res = { redirect }
+
+  post.returns(Promise.resolve())
+
+  try {
+    await postCheck(steps, config)(req, res, next)
+    t.equal(req.session.nextAllowedStep, CONFIRM_URL, `it sets next allowed step to ${CONFIRM_URL}`)
+    t.equal(redirect.called, true, 'it calls redirect()')
     t.end()
   } catch (error) {
     t.fail(error)
