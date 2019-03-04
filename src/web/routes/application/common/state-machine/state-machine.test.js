c0d0a8f5aa20fc778577e34c6456b50c2822efe3
Gethyn Jones <gethyn1@gmail.com>
2019-03-04 13:49:30 +0000

    HTBHF-629 extend state machine tests for COMPLETE

--- src/web/routes/application/common/state-machine/state-machine.test.js
+++ src/web/routes/application/common/state-machine/state-machine.test.js
@@ -1,6 +1,6 @@
 const test = require('tape')
 const { stateMachine, states, actions } = require('./state-machine')
-const { CHECK_URL } = require('../../common/constants')
+const { CHECK_URL, CONFIRM_URL } = require('../../common/constants')
 
 const { GET_NEXT_PATH } = actions
 
@@ -13,16 +13,23 @@ test(`Dispatching ${GET_NEXT_PATH} should return next property of associated ste
   t.end()
 })
 
-test(`Dispatching ${GET_NEXT_PATH} should return should return next property of associated step when state of IN_PROGRESS defined in session`, async (t) => {
+test(`Dispatching ${GET_NEXT_PATH} should return should return next property of associated step when state of ${states.IN_PROGRESS} defined in session`, async (t) => {
   const req = { method: 'POST', session: { state: states.IN_PROGRESS } }
 
   t.equal(stateMachine.dispatch(GET_NEXT_PATH, req, steps, '/first'), '/second')
   t.end()
 })
 
-test(`Dispatching ${GET_NEXT_PATH} should return check path when state of IN_REVIEW defined in session`, async (t) => {
+test(`Dispatching ${GET_NEXT_PATH} should return check path when state of ${states.IN_REVIEW} defined in session`, async (t) => {
   const req = { method: 'POST', session: { state: states.IN_REVIEW } }
 
   t.equal(stateMachine.dispatch(GET_NEXT_PATH, req, steps, '/first'), CHECK_URL)
+  t.end()
+})
+
+test(`Dispatching ${GET_NEXT_PATH} should return confirm path when state of ${states.COMPLETED} defined in session`, async (t) => {
+  const req = { method: 'POST', session: { state: states.COMPLETED } }
+
+  t.equal(stateMachine.dispatch(GET_NEXT_PATH, req, steps, '/first'), CONFIRM_URL)
   t.end()
 })
