1ee0dade53d31d73f97196857697b52ce87fd312
Mike Smith <mike.smith@sescon.co.uk>
2019-03-08 14:28:22 +0000

    [NO-JIRA] Fix cookie acceptance test so that it makes sure that at least one table is found.

--- src/test/common/steps/cookie-steps.js
+++ src/test/common/steps/cookie-steps.js
@@ -38,5 +38,6 @@ async function checkAllCookiePageContentIsPresentAndCorrect () {
   expect(h2Text.toString().trim()).to.be.equal('Google Analytics cookie', 'expected cookies page H2 text to be correct')
 
   // Make sure that there is a table on the page (this is the specific cookie details)
-  await pages.cookies.findByClassName(GOV_UK_TABLE_CLASSNAME)
+  const allTables = await pages.cookies.findAllByClassName(GOV_UK_TABLE_CLASSNAME)
+  expect(allTables).to.have.lengthOf.at.least(1, 'expect to find at least one table on cookie page')
 }
