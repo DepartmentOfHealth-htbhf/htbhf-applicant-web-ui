2c2269340e4f6076163bbde84718e89492b6bbbd
Gethyn Jones <gethyn1@gmail.com>
2019-04-15 15:18:35 +0100

    HTBHF-978 render voucher entitlement response in template

--- src/web/routes/application/confirm.js
+++ src/web/routes/application/confirm.js
@@ -1,11 +1,30 @@
+const httpStatus = require('http-status-codes')
+const { path, isNil } = require('ramda')
 const { handleRequestForPath } = require('./middleware')
 const { CONFIRM_URL, ELIGIBLE } = require('./common/constants')
+const { wrapError } = require('./common/formatters')
 
-const getConfirmPage = (req, res) => {
+const toPounds = pence => (parseInt(pence, 10) / 100).toFixed(2)
+
+const isNilOrLessThanZero = value => isNil(value) || value <= 0
+
+const getConfirmPage = (req, res, next) => {
   if (req.session.eligibilityStatus === ELIGIBLE) {
+    const totalVoucherValueInPence = path(['session', 'voucherEntitlement', 'totalVoucherValueInPence'], req)
+
+    if (isNilOrLessThanZero(totalVoucherValueInPence)) {
+      const errorMessage = `Invalid voucher value in pence returned from claimant service for ${ELIGIBLE} status: ${totalVoucherValueInPence}`
+      return next(wrapError({
+        cause: new Error(errorMessage),
+        message: errorMessage,
+        statusCode: httpStatus.INTERNAL_SERVER_ERROR
+      }))
+    }
+
     return res.render('confirm', {
       title: req.t('confirm.title'),
-      subTitle: req.t('confirm.subTitle')
+      subTitle: req.t('confirm.subTitle', { totalVoucherValue: toPounds(totalVoucherValueInPence) }),
+      totalVoucherValueForFourWeeks: toPounds(totalVoucherValueInPence * 4)
     })
   }
 
@@ -22,6 +41,8 @@ const registerConfirmRoute = (config, steps, app) => {
 }
 
 module.exports = {
+  toPounds,
+  isNilOrLessThanZero,
   registerConfirmRoute,
   getConfirmPage
 }
