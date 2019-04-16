cacb61af1e35e01ae82f6c3ca1b13aa25be43bb7
Gethyn Jones <gethyn1@gmail.com>
2019-04-16 10:28:28 +0100

    HTBHF-978 extract check post helpers to seperate files

---
+++ src/web/routes/application/check/constants.js
@@ -0,0 +1,7 @@
+const CLAIMS_ENDPOINT = `/v1/claims`
+const NO_ELIGIBILITY_STATUS_MESSAGE = 'The claimant service did not return an eligibility status'
+
+module.exports = {
+  CLAIMS_ENDPOINT,
+  NO_ELIGIBILITY_STATUS_MESSAGE
+}
