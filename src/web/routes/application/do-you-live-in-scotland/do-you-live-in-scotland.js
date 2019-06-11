6af8b69d2d1edf8eea8ab3ad1139598c83a9f505
Gethyn Jones <gethyn1@gmail.com>
2019-06-11 16:51:21 +0100

    HTBHF-1550 add do you live in scotland page

---
+++ src/web/routes/application/do-you-live-in-scotland/do-you-live-in-scotland.js
@@ -0,0 +1,24 @@
+const contentSummary = (req) => ({
+  key: req.t('doYouLiveInScotland.summaryKey'),
+  value: req.t(req.session.claim.doYouLiveInScotland)
+})
+
+const pageContent = ({ translate }) => ({
+  title: translate('doYouLiveInScotland.title'),
+  heading: translate('doYouLiveInScotland.heading'),
+  buttonText: translate('buttons:continue'),
+  yes: translate('yes'),
+  no: translate('no')
+})
+
+const doYouLiveInScotland = {
+  path: '/do-you-live-in-scotland',
+  next: '/enter-dob',
+  template: 'do-you-live-in-scotland',
+  pageContent,
+  contentSummary
+}
+
+module.exports = {
+  doYouLiveInScotland
+}
