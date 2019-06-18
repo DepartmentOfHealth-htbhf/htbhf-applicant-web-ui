7e45d8e07e784938acb11e3ed8a1b18c66f6a058
Gethyn Jones <gethyn1@gmail.com>
2019-06-18 11:28:17 +0100

    HTBHF-1551 add ability for steps to be ignored when determining previous page

--- src/web/routes/application/middleware/get-previous-page.js
+++ src/web/routes/application/middleware/get-previous-page.js
@@ -1,15 +1,34 @@
-const getPreviousPage = (steps, step) => {
+const { isNil } = require('ramda')
+
+const getPreviousAllowedStep = (steps, index, session) => {
+  if (index === 0) {
+    throw new Error('No allowed back route found')
+  }
+
+  const previousStep = steps[index - 1]
+  const { isNavigable, path } = previousStep
+
+  if (isNil(isNavigable) || isNavigable(session)) {
+    return path
+  }
+
+  return getPreviousAllowedStep(steps, index - 1, session)
+}
+
+const getPreviousPage = (steps, step, session) => {
   const index = steps.indexOf(step)
 
   if (index === -1) {
-    throw new Error(`Unable to find ${step} in the list of steps`)
+    throw new Error(`Unable to find ${JSON.stringify(step)} in the list of steps`)
   }
 
   // first page doesn't have a back link
   if (index > 0) {
-    const previousStep = steps[index - 1]
-    return previousStep.path
+    return getPreviousAllowedStep(steps, index, session)
   }
 }
 
-module.exports = getPreviousPage
+module.exports = {
+  getPreviousAllowedStep,
+  getPreviousPage
+}
