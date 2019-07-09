eda5529301b5709b703d47cedfa8d88529fa1769
David Wybourn <david.wybourn@gmail.com>
2019-07-09 11:46:21 +0100

    HTBHF-1702 Now validating that the confirmation code added is correct and sending a random code

--- src/web/routes/application/enter-code/validate.js
+++ src/web/routes/application/enter-code/validate.js
@@ -1,9 +1,11 @@
+const { CONFIRMATION_CODE_SESSION_PROPERTY } = require('../common/constants')
 const { check } = require('express-validator')
 const { translateValidationMessage } = require('../common/translate-validation-message')
 
+const validateConfirmationCodeIsCorrect = () => (confirmationCode, { req }) => req.body.confirmationCode === req.session[CONFIRMATION_CODE_SESSION_PROPERTY]
+
 const validate = [
-  check('confirmationCode')
-    .not().isEmpty()
+  check('confirmationCode').custom(validateConfirmationCodeIsCorrect())
     .withMessage(translateValidationMessage('validation:enterTheSixDigitCodeWeSentYou'))
 ]
 
