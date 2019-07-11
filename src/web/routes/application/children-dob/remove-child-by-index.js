cfcf7688d1683de5f5040599344daad967b8fba8
Gethyn Jones <gethyn1@gmail.com>
2019-07-11 15:03:11 +0100

    HTBHF-1680 add remove child by index function

--- src/web/routes/application/children-dob/remove-child-by-index.js
+++ src/web/routes/application/children-dob/remove-child-by-index.js
@@ -1,4 +1,4 @@
-const { isNil } = require('ramda')
+const { compose, map, isNil, pickBy, toPairs, fromPairs } = require('ramda')
 
 const INDEX_FROM_KEY_REG_EXP = /.*-(\d+).*/
 
@@ -24,7 +24,37 @@ const decrementKey = (key) => {
   return key.replace(index, index - 1)
 }
 
+const keyDoesNotContainIndex = (index) => (val, key) => getIndexFromKey(key) !== index
+
+const handleDecrement = (index) => (pair) => {
+  const key = pair[0]
+
+  if (isKeyAfterRemoved(key, index)) {
+    return [decrementKey(key), pair[1]]
+  }
+
+  return pair
+}
+
+const reIndexChildren = (children, index) => compose(fromPairs, map(handleDecrement(index)), toPairs)(children)
+
+const removeChildByIndex = (children, index) => {
+  const childDobPrefix = 'childDob-'
+
+  const metaEntries = pickBy((val, key) => !key.startsWith(childDobPrefix), children)
+  const childEntries = pickBy((val, key) => key.startsWith(childDobPrefix), children)
+
+  const filteredChildren = pickBy(keyDoesNotContainIndex(index), childEntries)
+
+  return {
+    ...reIndexChildren(filteredChildren, index),
+    ...metaEntries
+  }
+}
+
 module.exports = {
   isKeyAfterRemoved,
-  decrementKey
+  decrementKey,
+  keyDoesNotContainIndex,
+  removeChildByIndex
 }
