ff96962bc983812815091e90d89750451c32c83c
Mike Smith <mike.smith@sescon.co.uk>
2019-07-12 10:31:00 +0100

    HTBHF-1680 Add extra unit test permutations plus renamed method and constant from review comments.

--- src/web/routes/application/children-dob/selectors.js
+++ src/web/routes/application/children-dob/selectors.js
@@ -5,10 +5,10 @@ const getNonChildEntries = pickBy(isNotChildEntry)
 
 const getChildEntries = pickBy(isChildEntry)
 
-const getEntriesWithoutIndex = (entries, index) => pickBy(keyDoesNotContainIndex(index), entries)
+const omitKeysWithIndex = (entries, index) => pickBy(keyDoesNotContainIndex(index), entries)
 
 module.exports = {
   getChildEntries,
   getNonChildEntries,
-  getEntriesWithoutIndex
+  omitKeysWithIndex
 }
