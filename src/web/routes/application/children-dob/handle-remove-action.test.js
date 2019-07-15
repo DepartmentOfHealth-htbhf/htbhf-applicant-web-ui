e2b8af13abd3ac5b2f72a4de23ffe742a9f1033f
Gethyn Jones <gethyn1@gmail.com>
2019-07-15 14:17:39 +0100

    HTBHF-1680 enable remove action for children's DOB

---
+++ src/web/routes/application/children-dob/handle-remove-action.test.js
@@ -0,0 +1,161 @@
+const test = require('tape')
+const sinon = require('sinon')
+const { handleRemoveAction } = require('./handle-remove-action')
+
+const child = {
+  'childDobName-1': 'Lisa',
+  'childDob-day-1': '14',
+  'childDob-month-1': '11',
+  'childDob-year-1': '1990'
+}
+
+const newChild = {
+  'childDobName-2': 'Bart',
+  'childDob-day-2': '2',
+  'childDob-month-2': '3',
+  'childDob-year-2': '2001'
+}
+
+test('handleRemoveAction() removes child’s DOB from session on remove action', (t) => {
+  const req = {
+    session: {
+      children: {
+        ...child,
+        ...newChild,
+        childCount: 2,
+        inputCount: 2
+      }
+    },
+    body: {
+      ...child,
+      ...newChild,
+      remove: '1'
+    }
+  }
+
+  const redirect = sinon.spy()
+
+  const res = {
+    redirect,
+    locals: {}
+  }
+
+  const next = sinon.spy()
+
+  const expected = {
+    'childDobName-1': 'Bart',
+    'childDob-day-1': '2',
+    'childDob-month-1': '3',
+    'childDob-year-1': '2001',
+    'childCount': 1,
+    'inputCount': 1
+  }
+
+  handleRemoveAction(req, res, next)
+
+  t.deepEqual(req.session.children, expected, 'removes child’s DOB from session on remove action')
+  t.equal(redirect.calledWith('/children-dob'), true, 'redirects on remove action')
+  t.equal(next.called, false, 'does not call next')
+  t.end()
+})
+
+test('handleRemoveAction() does nothing when no remove action', (t) => {
+  const req = {
+    session: {
+      children: {
+        ...child,
+        childCount: 1,
+        inputCount: 1
+      }
+    },
+    body: {
+      ...child
+    }
+  }
+
+  const redirect = sinon.spy()
+
+  const res = {
+    redirect,
+    locals: {}
+  }
+
+  const next = sinon.spy()
+
+  const expected = {
+    'childDobName-1': 'Lisa',
+    'childDob-day-1': '14',
+    'childDob-month-1': '11',
+    'childDob-year-1': '1990',
+    'childCount': 1,
+    'inputCount': 1
+  }
+
+  handleRemoveAction(req, res, next)
+
+  t.deepEqual(req.session.children, expected, 'does not mutate children')
+  t.equal(redirect.calledWith('/children-dob'), false, 'does not redirect')
+  t.equal(next.called, true, 'calls next')
+  t.end()
+})
+
+test('handleRemoveAction() removes child’s DOB from session on remove action when only one child exists', (t) => {
+  const req = {
+    session: {
+      children: {
+        ...child,
+        childCount: 1,
+        inputCount: 1
+      }
+    },
+    body: {
+      ...child,
+      remove: '1'
+    }
+  }
+
+  const redirect = sinon.spy()
+
+  const res = {
+    redirect,
+    locals: {}
+  }
+
+  const next = sinon.spy()
+
+  const expected = {
+    'childCount': 0,
+    'inputCount': 1
+  }
+
+  handleRemoveAction(req, res, next)
+
+  t.deepEqual(req.session.children, expected, 'removes child’s DOB from session on remove action')
+  t.equal(redirect.calledWith('/children-dob'), true, 'redirects on remove action')
+  t.equal(next.called, false, 'does not call next')
+  t.end()
+})
+
+test('handleRemoveAction() throws an error on remove action when index is less than 1', (t) => {
+  const req = {
+    session: {
+      children: {
+        childCount: 0,
+        inputCount: 1
+      }
+    },
+    body: {
+      remove: '0'
+    }
+  }
+
+  const redirect = sinon.spy()
+
+  const res = {
+    redirect,
+    locals: {}
+  }
+
+  t.throws(() => handleRemoveAction(req, res, () => {}), /Unable to remove index 0/)
+  t.end()
+})
---
+++ src/web/routes/application/children-dob/set-children-in-session.js
@@ -0,0 +1,34 @@
+const { pickBy } = require('ramda')
+const { countKeysContainingString } = require('./count-keys')
+const { DAY_FIELD_SUFFIX } = require('./constants')
+const { isChildEntry } = require('./predicates')
+
+const setChildrenInSessionForGet = (req) => {
+  if (!req.session.hasOwnProperty('children')) {
+    req.session.children = {
+      inputCount: 1,
+      childCount: 0
+    }
+  }
+
+  return req
+}
+
+const setChildrenInSessionForPost = (req, res, next) => {
+  const childCount = countKeysContainingString(DAY_FIELD_SUFFIX, req.body)
+
+  req.session.children = {
+    ...pickBy(isChildEntry, req.body),
+    inputCount: childCount,
+    childCount: childCount
+  }
+
+  // GET behaviour is not called on validation error so locals also need setting for POST
+  res.locals.children = req.session.children
+  return next()
+}
+
+module.exports = {
+  setChildrenInSessionForGet,
+  setChildrenInSessionForPost
+}
