75189c7ad7d2b34ab4c3cf0e52508a73f496c45d
Gethyn Jones <gethyn1@gmail.com>
2019-07-29 13:19:43 +0100

    HTBHF-1909 rename template caching variable

--- src/config/server.js
+++ src/config/server.js
@@ -4,5 +4,5 @@ module.exports = {
   PORT: process.env.PORT,
   SESSION_SECRET: process.env.SESSION_SECRET,
   SESSION_ID_NAME: 'htbhf.sid',
-  CACHE_VIEW_TEMPLATES: toBoolean(process.env.CACHE_VIEW_TEMPLATES)
+  NO_CACHE_VIEW_TEMPLATES: toBoolean(process.env.NO_CACHE_VIEW_TEMPLATES)
 }
