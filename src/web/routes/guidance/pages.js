e606e41a5605457a969e4c01aa860c2c14e462d4
Gethyn Jones <gethyn1@gmail.com>
2019-08-01 09:25:58 +0100

    HTBHF-1953 import guidance page objects as constant

---
+++ src/web/routes/guidance/pages.js
@@ -0,0 +1,30 @@
+module.exports.PAGES = [
+  {
+    title: 'How it works',
+    path: '/how-it-works'
+  },
+  {
+    title: 'Eligibility',
+    path: '/eligibility'
+  },
+  {
+    title: 'What you get',
+    path: '/what-you-get'
+  },
+  {
+    title: 'What you can buy',
+    path: '/what-you-can-buy'
+  },
+  {
+    title: 'Using your card',
+    path: '/using-your-card'
+  },
+  {
+    title: 'Apply for Healthy Start',
+    path: '/apply-for-healthy-start'
+  },
+  {
+    title: 'Report a change',
+    path: '/report-a-change'
+  }
+]
