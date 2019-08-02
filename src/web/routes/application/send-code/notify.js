b6a4c92ba08eb0a7f3a8a37679f1ee87ff993ebb
Matt Smith <msmith@equalexperts.com>
2019-08-02 11:44:12 +0100

    HTBHF-1982 Added failure event logging if we cannot call notify

--- src/web/routes/application/send-code/notify.js
+++ src/web/routes/application/send-code/notify.js
@@ -6,13 +6,24 @@ const { logger } = require('../../../logger')
 
 const { EMAIL, TEXT } = require('../common/constants')
 
+const SEND_2FA_CODE = 'SEND_2FA_CODE'
+
 // template ids matching the templates in govuk notify.
 const EMAIL_TEMPLATE_ID = '18cf4f69-e1b5-4aa9-bed7-c906d3a59285'
 const SMS_TEMPLATE_ID = 'a770479a-097c-4079-acc2-d452a6f21585'
 
-function logConfirmationCodeEvent (channel, reference) {
-  const data = { eventType: 'SEND_2FA_CODE', timestamp: new Date(), codeChannel: channel, notifyReference: reference }
-  logger.info(JSON.stringify(data))
+function logFailureEvent (initialEvent, failureDescription, exceptionDetail) {
+  // event log format must be eventType, then timestamp, then other attributes
+  const data = {
+    eventType: 'FAILURE',
+    timestamp: initialEvent.timestamp,
+    failedEvent: initialEvent.eventType,
+    failureDescription,
+    codeChannel: initialEvent.codeChannel,
+    notifyReference: initialEvent.notifyReference,
+    exceptionDetail: exceptionDetail.message
+  }
+  logger.error(JSON.stringify(data))
 }
 
 function sendConfirmationCode (claim, channel, code) {
@@ -20,12 +31,15 @@ function sendConfirmationCode (claim, channel, code) {
   const reference = uuid()
   const notifyOptions = { personalisation: { confirmationCode: code }, reference: reference }
 
-  logConfirmationCodeEvent(channel, reference)
+  const logData = { eventType: SEND_2FA_CODE, timestamp: new Date(), codeChannel: channel, notifyReference: reference }
+  logger.info(JSON.stringify(logData))
 
   if (channel === EMAIL) {
     notifyClient.sendEmail(EMAIL_TEMPLATE_ID, claim.emailAddress, notifyOptions)
+      .catch(e => { logFailureEvent(logData, 'Error sending email', e) })
   } else if (channel === TEXT) {
     notifyClient.sendSms(SMS_TEMPLATE_ID, claim.phoneNumber, notifyOptions)
+      .catch(e => { logFailureEvent(logData, 'Error sending SMS', e) })
   } else {
     throw new Error(`Expecting 'channelForCode' option to be either 'text' or 'email', instead was ${channel}`)
   }
--- src/web/routes/application/send-code/notify.test.js
+++ src/web/routes/application/send-code/notify.test.js
@@ -4,16 +4,39 @@ const sinon = require('sinon')
 
 const { TEXT, EMAIL } = require('../common/constants')
 
+const ERROR_PHONE_NUMBER = '09999999999'
+const ERROR_EMAIL = 'error@mail.com'
+const ERROR_MESSAGE = 'failure to notify'
+
 const sendSms = sinon.spy()
 const sendEmail = sinon.spy()
+const info = sinon.spy()
+const error = sinon.spy()
 
 class NotifyClient {
-  sendSms (templateId, phoneNumber, options) { sendSms(templateId, phoneNumber, options) }
+  sendSms (templateId, phoneNumber, options) {
+    if (phoneNumber === ERROR_PHONE_NUMBER) {
+      return Promise.reject(new Error(ERROR_MESSAGE))
+    }
+    sendSms(templateId, phoneNumber, options)
+    return Promise.resolve()
+  }
 
-  sendEmail (templateId, emailAddress, options) { sendEmail(templateId, emailAddress, options) }
+  sendEmail (templateId, emailAddress, options) {
+    if (emailAddress === ERROR_EMAIL) {
+      return Promise.reject(new Error(ERROR_MESSAGE))
+    }
+    sendEmail(templateId, emailAddress, options)
+    return Promise.resolve()
+  }
 }
 
-const { sendConfirmationCode, SMS_TEMPLATE_ID, EMAIL_TEMPLATE_ID } = proxyquire('./notify', { 'notifications-node-client': { NotifyClient } })
+const logger = { info, error }
+
+const { sendConfirmationCode, SMS_TEMPLATE_ID, EMAIL_TEMPLATE_ID } = proxyquire('./notify', {
+  'notifications-node-client': { NotifyClient },
+  '../../../logger': { logger }
+})
 
 test('sendCodeToSMS', (t) => {
   const claim = {
@@ -56,5 +79,41 @@ test('throwErrorWhenChannelForCodeNotEmailOrText', (t) => {
   const confirmationCode = '123456'
 
   t.throws(sendConfirmationCode.bind(null, claim, channelForCode, confirmationCode), /Expecting 'channelForCode' option to be either 'text' or 'email', instead was other/)
+  t.end()
+})
+
+test('sendConfirmationCode to SMS logs error', async (t) => {
+  const claim = {
+    phoneNumber: ERROR_PHONE_NUMBER,
+    emailAddress: 'test@email.com'
+  }
+  const channelForCode = TEXT
+  const confirmationCode = '123456'
+
+  await sendConfirmationCode(claim, channelForCode, confirmationCode)
+  const errorLog = error.getCall(0).args[0]
+  const errorObj = JSON.parse(errorLog)
+
+  t.equal(errorObj.eventType, 'FAILURE')
+  t.equal(errorObj.failedEvent, 'SEND_2FA_CODE')
+  t.equal(errorObj.exceptionDetail, ERROR_MESSAGE)
+  t.end()
+})
+
+test('sendConfirmationCode to email logs error', async (t) => {
+  const claim = {
+    phoneNumber: '07123456789',
+    emailAddress: ERROR_EMAIL
+  }
+  const channelForCode = EMAIL
+  const confirmationCode = '123456'
+
+  await sendConfirmationCode(claim, channelForCode, confirmationCode)
+  const errorLog = error.getCall(0).args[0]
+  const errorObj = JSON.parse(errorLog)
+
+  t.equal(errorObj.eventType, 'FAILURE')
+  t.equal(errorObj.failedEvent, 'SEND_2FA_CODE')
+  t.equal(errorObj.exceptionDetail, ERROR_MESSAGE)
   t.end()
 })
