849c03a03571724f7ebbe51d76deffdfa2357bbe
Mike Smith <mike.smith@sescon.co.uk>
2019-08-02 14:38:41 +0100

    HTBHF-1964 Fix requires after merge with story branch, plus fixes from review comments.

--- src/web/routes/guidance/index.js
+++ src/web/routes/guidance/index.js
@@ -1,3 +1,7 @@
+const { getPageMetadata } = require('./get-page-meta-data')
+
 module.exports = {
-  ...require('./guidance')
+  ...require('./guidance'),
+  ...require('./pages'),
+  getPageMetadata
 }
