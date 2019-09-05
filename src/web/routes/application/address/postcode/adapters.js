7ec35b96c4663b1d71e57068e9fce0aaa7a9788f
Matt Smith <matthew.smith@thinkinglogic.com>
2019-09-05 15:25:31 +0100

    HTBHF-2203 Converted addresses from OS Places to title case

--- src/web/routes/application/address/postcode/adapters.js
+++ src/web/routes/application/address/postcode/adapters.js
@@ -2,14 +2,31 @@ const { compose, map, pick, prop, propOr } = require('ramda')
 const { OS_PLACES_ADDRESS_KEYS } = require('../constants')
 
 const RESULTS_PROP = 'results'
-
 const DELIVERY_POINT_ADDRESS_PROP = 'DPA'
+const OS_PLACES_TITLE_CASE_KEYS = OS_PLACES_ADDRESS_KEYS.filter(key => !['ADDRESS', 'POSTCODE'].includes(key))
 
-const transformAddress = compose(pick(OS_PLACES_ADDRESS_KEYS), prop(DELIVERY_POINT_ADDRESS_PROP))
+const toTitleCase = (str) => {
+  return str.replace(
+    /\b\w+/g,
+    function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() }
+  )
+}
+
+const convertCase = (address) => {
+  const titleCase = compose(map(toTitleCase), pick(OS_PLACES_TITLE_CASE_KEYS))(address)
+  const postcode = address.POSTCODE
+  titleCase['ADDRESS'] = toTitleCase(address.ADDRESS.replace(`, ${postcode}`, ''))
+  titleCase['POSTCODE'] = postcode
+  return titleCase
+}
+
+const transformAddress = compose(convertCase, pick(OS_PLACES_ADDRESS_KEYS), prop(DELIVERY_POINT_ADDRESS_PROP))
 
 const transformOsPlacesApiResponse = compose(map(transformAddress), propOr([], RESULTS_PROP))
 
 module.exports = {
+  toTitleCase,
+  convertCase,
   transformAddress,
   transformOsPlacesApiResponse
 }
--- src/web/routes/application/address/postcode/adapters.test.js
+++ src/web/routes/application/address/postcode/adapters.test.js
@@ -1,19 +1,44 @@
 const test = require('tape')
-const { transformAddress, transformOsPlacesApiResponse } = require('./adapters')
+const { transformAddress, transformOsPlacesApiResponse, toTitleCase, convertCase } = require('./adapters')
 const TEST_FIXTURES = require('./test-fixtures.json')
+
+test('toTitleCase() should uppercase the first letter of every word', (t) => {
+  const result = toTitleCase('10A, MY STREET, WESTON-SUPER-MARE')
+
+  const expected = '10a, My Street, Weston-Super-Mare'
+
+  t.equals(result, expected, 'transforms string to title case correctly')
+  t.end()
+})
+
+test('convertCase() should convert the case of the address to title case', (t) => {
+  const result = convertCase(TEST_FIXTURES.results[4].DPA)
+
+  const expected = {
+    ADDRESS: '10a, Mayfield Avenue, Weston-Super-Mare',
+    BUILDING_NAME: '10a',
+    THOROUGHFARE_NAME: 'Mayfield Avenue',
+    POST_TOWN: 'Weston-Super-Mare',
+    POSTCODE: 'BS22 6AA',
+    LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'North Somerset'
+  }
+
+  t.deepEqual(result, expected, 'converts case of an address correctly')
+  t.end()
+})
 
 test('transformAddress() transforms address correctly', (t) => {
   const result = transformAddress(TEST_FIXTURES.results[0])
 
   const expected = {
-    ADDRESS: 'ALAN JEFFERY ENGINEERING, 1, VALLEY ROAD, PLYMOUTH, PL7 1RF',
-    ORGANISATION_NAME: 'ALAN JEFFERY ENGINEERING',
+    ADDRESS: 'Alan Jeffery Engineering, 1, Valley Road, Plymouth',
+    ORGANISATION_NAME: 'Alan Jeffery Engineering',
     BUILDING_NUMBER: '1',
-    THOROUGHFARE_NAME: 'VALLEY ROAD',
-    DEPENDENT_THOROUGHFARE_NAME: 'UPPER VALLEY ROAD',
-    POST_TOWN: 'PLYMOUTH',
+    THOROUGHFARE_NAME: 'Valley Road',
+    DEPENDENT_THOROUGHFARE_NAME: 'Upper Valley Road',
+    POST_TOWN: 'Plymouth',
     POSTCODE: 'PL7 1RF',
-    LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'CITY OF PLYMOUTH'
+    LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'City Of Plymouth'
   }
 
   t.deepEqual(result, expected, 'transforms address correctly')
@@ -25,43 +50,50 @@ test('transformOsPlacesApiResponse() transforms OS Places API response correctly
 
   const expected = [
     {
-      ADDRESS: 'ALAN JEFFERY ENGINEERING, 1, VALLEY ROAD, PLYMOUTH, PL7 1RF',
-      ORGANISATION_NAME: 'ALAN JEFFERY ENGINEERING',
+      ADDRESS: 'Alan Jeffery Engineering, 1, Valley Road, Plymouth',
+      ORGANISATION_NAME: 'Alan Jeffery Engineering',
       BUILDING_NUMBER: '1',
-      THOROUGHFARE_NAME: 'VALLEY ROAD',
-      DEPENDENT_THOROUGHFARE_NAME: 'UPPER VALLEY ROAD',
-      POST_TOWN: 'PLYMOUTH',
+      THOROUGHFARE_NAME: 'Valley Road',
+      DEPENDENT_THOROUGHFARE_NAME: 'Upper Valley Road',
+      POST_TOWN: 'Plymouth',
       POSTCODE: 'PL7 1RF',
-      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'CITY OF PLYMOUTH'
+      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'City Of Plymouth'
     }, {
-      ADDRESS: 'DULUX DECORATOR CENTRE, 2, VALLEY ROAD, PLYMOUTH, PL7 1RF',
-      ORGANISATION_NAME: 'DULUX DECORATOR CENTRE',
+      ADDRESS: 'Dulux Decorator Centre, 2, Valley Road, Plymouth',
+      ORGANISATION_NAME: 'Dulux Decorator Centre',
       BUILDING_NUMBER: '2',
-      THOROUGHFARE_NAME: 'VALLEY ROAD',
-      DEPENDENT_THOROUGHFARE_NAME: 'UPPER VALLEY ROAD',
-      POST_TOWN: 'PLYMOUTH',
+      THOROUGHFARE_NAME: 'Valley Road',
+      DEPENDENT_THOROUGHFARE_NAME: 'Upper Valley Road',
+      POST_TOWN: 'Plymouth',
       POSTCODE: 'PL7 1RF',
-      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'CITY OF PLYMOUTH'
+      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'City Of Plymouth'
     }, {
-      ADDRESS: 'MILL AUTOS, 3, VALLEY ROAD, PLYMOUTH, PL7 1RF',
-      ORGANISATION_NAME: 'MILL AUTOS',
+      ADDRESS: 'Mill Autos, 3, Valley Road, Plymouth',
+      ORGANISATION_NAME: 'Mill Autos',
       BUILDING_NUMBER: '3',
-      THOROUGHFARE_NAME: 'VALLEY ROAD',
-      DEPENDENT_THOROUGHFARE_NAME: 'UPPER VALLEY ROAD',
-      POST_TOWN: 'PLYMOUTH',
+      THOROUGHFARE_NAME: 'Valley Road',
+      DEPENDENT_THOROUGHFARE_NAME: 'Upper Valley Road',
+      POST_TOWN: 'Plymouth',
       POSTCODE: 'PL7 1RF',
-      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'CITY OF PLYMOUTH'
+      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'City Of Plymouth'
     }, {
-      ADDRESS: 'GOAT HILL FARM, 2, TROLL BRIDGE, GOAT HILL, SLAITHWAITE, SLAITH, HUDDERSFIELD, HD7 5UZ',
-      ORGANISATION_NAME: 'GOAT HILL FARM',
+      ADDRESS: 'Goat Hill Farm, 2, Troll Bridge, Goat Hill, Slaithwaite, Slaith, Huddersfield',
+      ORGANISATION_NAME: 'Goat Hill Farm',
       BUILDING_NUMBER: '2',
-      THOROUGHFARE_NAME: 'GOAT HILL',
-      DEPENDENT_THOROUGHFARE_NAME: 'TROLL BRIDGE',
-      DOUBLE_DEPENDENT_LOCALITY: 'SLAITHWAITE',
-      DEPENDENT_LOCALITY: 'SLAITH',
-      POST_TOWN: 'HUDDERSFIELD',
+      THOROUGHFARE_NAME: 'Goat Hill',
+      DEPENDENT_THOROUGHFARE_NAME: 'Troll Bridge',
+      DOUBLE_DEPENDENT_LOCALITY: 'Slaithwaite',
+      DEPENDENT_LOCALITY: 'Slaith',
+      POST_TOWN: 'Huddersfield',
       POSTCODE: 'HD7 5UZ',
-      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'KIRKLEES'
+      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'Kirklees'
+    }, {
+      ADDRESS: '10a, Mayfield Avenue, Weston-Super-Mare',
+      BUILDING_NAME: '10a',
+      THOROUGHFARE_NAME: 'Mayfield Avenue',
+      POST_TOWN: 'Weston-Super-Mare',
+      POSTCODE: 'BS22 6AA',
+      LOCAL_CUSTODIAN_CODE_DESCRIPTION: 'North Somerset'
     }
   ]
 
