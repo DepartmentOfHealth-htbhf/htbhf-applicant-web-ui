b76c14d0fc3d876dd49ae1daf12ba6f98f1d3dde
Gethyn Jones <gethyn1@gmail.com>
2019-09-19 13:36:27 +0100

    HTBHF-2276 extract address keys to constants

--- src/web/routes/application/address/content-summary.js
+++ src/web/routes/application/address/content-summary.js
@@ -1,19 +1,16 @@
 const { join, filter, compose } = require('ramda')
 
 const { notIsNilOrEmpty } = require('../../../../common/predicates')
+const { ADDRESS_KEYS } = require('./constants')
 
 const newLineChar = '\n'
 const toMultiLineString = compose(join(newLineChar), filter(notIsNilOrEmpty))
 
+const getKeyFromClaim = (claim) => (key) => claim[key]
+
 const addressContentSummary = (req) => ({
   key: req.t('address.summaryKey'),
-  value: toMultiLineString([
-    req.session.claim.addressLine1,
-    req.session.claim.addressLine2,
-    req.session.claim.townOrCity,
-    req.session.claim.county,
-    req.session.claim.postcode
-  ])
+  value: toMultiLineString(ADDRESS_KEYS.map(getKeyFromClaim(req.session.claim)))
 })
 
 module.exports = {
--- src/web/routes/application/address/request-body.js
+++ src/web/routes/application/address/request-body.js
@@ -1,11 +1,9 @@
+const { ADDRESS_KEYS } = require('./constants')
+
+const getAddressFieldFromClaim = (claim) => (address, key) => ({ ...address, [key]: claim[key] })
+
 const requestBody = (session) => ({
-  address: {
-    addressLine1: session.claim.addressLine1,
-    addressLine2: session.claim.addressLine2,
-    townOrCity: session.claim.townOrCity,
-    county: session.claim.county,
-    postcode: session.claim.postcode
-  }
+  address: ADDRESS_KEYS.reduce(getAddressFieldFromClaim(session.claim), {})
 })
 
 module.exports = {
