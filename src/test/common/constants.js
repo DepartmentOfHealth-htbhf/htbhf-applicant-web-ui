550806e60f4910ffc610228d3910cf171cff49f6
Gethyn Jones <gethyn1@gmail.com>
2019-10-22 13:54:16 +0100

    NO-JIRA remove redundant wiremock mappings

--- src/test/common/constants.js
+++
@@ -1,2 +0,0 @@
-module.exports.ELIGIBLE = 'ELIGIBLE'
-module.exports.CLAIMS_ENDPOINT = '/v2/claims'
--- src/test/common/wiremock/claim/claim.js
+++ src/test/common/wiremock/claim/claim.js
@@ -1,32 +1,11 @@
 const { postJsonData } = require('../../request')
-const { ELIGIBLE } = require('../../constants')
 const { WIREMOCK_CLAIMANT_MAPPING_URL } = require('../paths')
-
-const {
-  createSuccessfulClaimsMapping,
-  createUpdatedClaimsMapping,
-  ERROR_CLAIMS_MAPPING
-} = require('./mappings')
+const { createSuccessfulClaimsMapping } = require('./mappings')
 
 async function setupSuccessfulWiremockClaimMapping () {
-  await postJsonData(WIREMOCK_CLAIMANT_MAPPING_URL, createSuccessfulClaimsMapping(ELIGIBLE))
-}
-
-async function setupSuccessfulWiremockUpdatedClaimMapping () {
-  await postJsonData(WIREMOCK_CLAIMANT_MAPPING_URL, createUpdatedClaimsMapping())
-}
-
-async function setupSuccessfulWiremockClaimMappingWithStatus (status) {
-  await postJsonData(WIREMOCK_CLAIMANT_MAPPING_URL, createSuccessfulClaimsMapping(status))
-}
-
-async function setupErrorWiremockClaimMapping () {
-  await postJsonData(WIREMOCK_CLAIMANT_MAPPING_URL, ERROR_CLAIMS_MAPPING)
+  await postJsonData(WIREMOCK_CLAIMANT_MAPPING_URL, createSuccessfulClaimsMapping())
 }
 
 module.exports = {
-  setupSuccessfulWiremockClaimMappingWithStatus,
-  setupSuccessfulWiremockUpdatedClaimMapping,
-  setupErrorWiremockClaimMapping,
   setupSuccessfulWiremockClaimMapping
 }
