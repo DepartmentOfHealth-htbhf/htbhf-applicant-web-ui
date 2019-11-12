0827e7ed06ad999fadc9027744b097ef5a0d916f
Gethyn Jones <gethyn1@gmail.com>
2019-11-12 11:47:55 +0000

    HTBHF-2548 modify state machine to operate on session by journey

--- src/web/routes/application/flow-control/state-machine/state-machine.js
+++ src/web/routes/application/flow-control/state-machine/state-machine.js
@@ -3,47 +3,50 @@ const { logger } = require('../../../../logger')
 const states = require('./states')
 const { isPathAllowed } = require('./predicates')
 const { getNextNavigablePath, getNextInReviewPath } = require('./selectors')
-const { setNextAllowedPath } = require('./session-accessors')
+const {
+  setNextAllowedPathInSession,
+  setStateInSession,
+  getNextAllowedPathFromSession,
+  getStateFromSession
+} = require('./session-accessors')
 
 const { IN_PROGRESS, IN_REVIEW, COMPLETED } = states
 
 const stateMachine = {
   [IN_PROGRESS]: {
     getNextPath: (req, journey) => getNextNavigablePath(req.path, req, journey.steps),
-    isPathAllowed: (req, journey) => isPathAllowed(journey.pathsInSequence, req.session.nextAllowedStep, req.path),
-    getNextAllowedPath: (req) => req.session.nextAllowedStep,
-    setNextAllowedPath,
-    incrementNextAllowedPath: (req, journey) => setNextAllowedPath(req, journey, getNextNavigablePath(req.path, req, journey.steps))
+    isPathAllowed: (req, journey) => isPathAllowed(journey.pathsInSequence, getNextAllowedPathFromSession(req, journey), req.path),
+    getNextAllowedPath: getNextAllowedPathFromSession,
+    setNextAllowedPath: setNextAllowedPathInSession,
+    incrementNextAllowedPath: (req, journey) => setNextAllowedPathInSession(req, journey, getNextNavigablePath(req.path, req, journey.steps))
   },
   [IN_REVIEW]: {
     getNextPath: getNextInReviewPath,
-    isPathAllowed: (req, journey) => isPathAllowed(journey.pathsInSequence, req.session.nextAllowedStep, req.path),
-    getNextAllowedPath: (req) => req.session.nextAllowedStep,
-    setNextAllowedPath,
-    incrementNextAllowedPath: (req, journey) => setNextAllowedPath(req, journey, getNextInReviewPath(req)),
-    invalidateReview: (req) => stateMachine.setState(IN_PROGRESS, req)
+    isPathAllowed: (req, journey) => isPathAllowed(journey.pathsInSequence, getNextAllowedPathFromSession(req, journey), req.path),
+    getNextAllowedPath: getNextAllowedPathFromSession,
+    setNextAllowedPath: setNextAllowedPathInSession,
+    incrementNextAllowedPath: (req, journey) => setNextAllowedPathInSession(req, journey, getNextInReviewPath(req)),
+    invalidateReview: (req, journey) => setStateInSession(req, journey, IN_PROGRESS)
   },
   [COMPLETED]: {
     getNextPath: () => CONFIRM_URL,
     isPathAllowed: (req) => req.path === CONFIRM_URL,
     getNextAllowedPath: () => CONFIRM_URL,
-    setNextAllowedPath,
-    incrementNextAllowedPath: (req, journey) => setNextAllowedPath(req, journey, CONFIRM_URL)
+    setNextAllowedPath: setNextAllowedPathInSession,
+    incrementNextAllowedPath: (req, journey) => setNextAllowedPathInSession(req, journey, CONFIRM_URL)
   },
 
-  getState: (req) => {
-    return req.session.hasOwnProperty('state') ? states[req.session.state] : IN_PROGRESS
-  },
+  getState: getStateFromSession,
 
-  setState: (state, req) => {
-    if (stateMachine.getState(req) !== state) {
+  setState: (state, req, journey) => {
+    if (getStateFromSession(req, journey) !== state) {
       logger.info(`State set to ${state}`, { req })
-      req.session.state = state
+      setStateInSession(req, journey, state)
     }
   },
 
   dispatch: (actionType, req, journey, ...args) => {
-    const state = stateMachine.getState(req)
+    const state = getStateFromSession(req, journey)
     const action = stateMachine[state][actionType]
 
     if (typeof action !== 'undefined') {
