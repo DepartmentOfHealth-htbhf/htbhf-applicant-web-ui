99216ed350c423df5516f803d46c559c412549cd
Gethyn Jones <gethyn1@gmail.com>
2019-11-18 15:21:48 +0000

    HTBHF-2598 set default session state for static routes

--- src/web/routes/application/steps/check-answers/check-answers.js
+++ src/web/routes/application/steps/check-answers/check-answers.js
@@ -1,11 +1,11 @@
 const { getCheckAnswers } = require('./get')
 const { CHECK_ANSWERS_URL, prefixPath } = require('../../paths')
-const { handleRequestForPath } = require('../../flow-control')
+const { configureSessionDetails, handleRequestForPath } = require('../../flow-control')
 
 const registerCheckAnswersRoutes = (journey, app) => {
   app
     .route(prefixPath(journey.pathPrefix, CHECK_ANSWERS_URL))
-    .get(handleRequestForPath(journey), getCheckAnswers(journey))
+    .get(configureSessionDetails(journey), handleRequestForPath(journey), getCheckAnswers(journey))
 }
 
 module.exports = {
