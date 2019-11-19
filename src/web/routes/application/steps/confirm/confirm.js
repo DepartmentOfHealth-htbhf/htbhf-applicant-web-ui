370d6ffa7645030cbba24eed1d3c4704520ae855
Gethyn Jones <gethyn1@gmail.com>
2019-11-19 09:25:58 +0000

    HTBHF-2598 set default session state for static routes (#617)

--- src/web/routes/application/steps/confirm/confirm.js
+++ src/web/routes/application/steps/confirm/confirm.js
@@ -1,6 +1,6 @@
 const httpStatus = require('http-status-codes')
 const { path, isNil } = require('ramda')
-const { handleRequestForPath } = require('../../flow-control')
+const { configureSessionDetails, handleRequestForPath } = require('../../flow-control')
 const { CONFIRM_URL, prefixPath } = require('../../paths')
 const { ELIGIBLE } = require('../common/constants')
 const { wrapError } = require('../../errors')
@@ -49,9 +49,8 @@ const getConfirmPage = (req, res, next) => {
   })
 }
 
-const registerConfirmRoute = (journey, app) => {
-  app.get(prefixPath(journey.pathPrefix, CONFIRM_URL), handleRequestForPath(journey), getConfirmPage)
-}
+const registerConfirmRoute = (journey, app) =>
+  app.get(prefixPath(journey.pathPrefix, CONFIRM_URL), configureSessionDetails(journey), handleRequestForPath(journey), getConfirmPage)
 
 module.exports = {
   toPounds,
