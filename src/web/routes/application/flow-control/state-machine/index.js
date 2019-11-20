f0951aa0bbb0108bd355ee3e198dc9882ee9267a
Gethyn Jones <gethyn1@gmail.com>
2019-11-20 15:57:58 +0000

    HTBHF-2507 move states to top level of flow control directory

--- src/web/routes/application/flow-control/state-machine/index.js
+++ src/web/routes/application/flow-control/state-machine/index.js
@@ -1,9 +1,7 @@
 const { stateMachine } = require('./state-machine')
-const states = require('./states')
 const actions = require('./actions')
 
 module.exports = {
-  states,
   actions,
   stateMachine
 }
