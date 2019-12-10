8cac14a093ae8d5136803aa35228d8babce0c8f8
Gethyn Jones <gethyn1@gmail.com>
2019-12-10 14:23:00 +0000

    HTBHF-2696 render instant failure decision template

--- src/web/routes/application/steps/decision/decision.js
+++ src/web/routes/application/steps/decision/decision.js
@@ -1,15 +1,42 @@
 const { configureSessionDetails, handleRequestForPath } = require('../../flow-control')
 const { DECISION_URL, prefixPath } = require('../../paths')
+const { isUndefined } = require('../../../../../common/predicates')
+const { getDecisionStatus } = require('./get-decision-status')
+const { FAIL } = require('./statuses')
 const { getDecisionPageFallback } = require('./decision-fallback')
+
+const STATUS_TEMPLATE_MAP = {
+  [FAIL]: 'failure'
+}
+
+const getRenderArgsForStatus = (req, status) => {
+  const template = STATUS_TEMPLATE_MAP[status]
+  return [`decision/${template}`, {
+    title: req.t(`decision.${template}.title`),
+    body: req.t(`decision.${template}.body`)
+  }]
+}
+
+// TODO: Remove references to "decision fallback" once all stories for epic HTBHF-2014 (receive my decision) are complete.
+// After removal of fallback, update handler to throw error if `decisionStatus` is undefined.
+const getDecisionPage = (req, res, next) => {
+  const decisionStatus = getDecisionStatus(req.session.verificationResult)
+
+  return isUndefined(decisionStatus)
+    ? next()
+    : res.render(...getRenderArgsForStatus(req, decisionStatus))
+}
 
 const registerDecisionRoute = (journey, app) =>
   app.get(
     prefixPath(journey.pathPrefix, DECISION_URL),
     configureSessionDetails(journey),
     handleRequestForPath(journey),
+    getDecisionPage,
     getDecisionPageFallback
   )
 
 module.exports = {
-  registerDecisionRoute
+  registerDecisionRoute,
+  getDecisionPage
 }
---
+++ src/web/routes/application/steps/decision/decision.test.js
@@ -0,0 +1,68 @@
+const test = require('tape')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+const { identity } = require('ramda')
+const { FAIL } = require('./statuses')
+
+const getDecisionStatus = sinon.stub()
+
+const { getDecisionPage } = proxyquire('./decision', {
+  './get-decision-status': {
+    getDecisionStatus
+  }
+})
+
+const resetStubs = () => {
+  getDecisionStatus.reset()
+}
+
+test('getDecisionPage() calls next if decision status is undefined', (t) => {
+  getDecisionStatus.returns(undefined)
+
+  const render = sinon.spy()
+  const next = sinon.spy()
+
+  const req = {
+    t: identity,
+    session: {
+      verificationResult: {}
+    }
+  }
+
+  const res = {
+    render
+  }
+
+  getDecisionPage(req, res, next)
+
+  t.equal(next.called, true, 'it calls next()')
+  t.equal(render.called, false, 'it does not call render()')
+  resetStubs()
+  t.end()
+})
+
+test(`getDecisionPage() renders failure view if decision status is ${FAIL}`, (t) => {
+  getDecisionStatus.returns(FAIL)
+
+  const render = sinon.spy()
+  const next = sinon.spy()
+
+  const req = {
+    t: identity,
+    language: 'en',
+    session: {
+      verificationResult: {}
+    }
+  }
+
+  const res = {
+    render
+  }
+
+  getDecisionPage(req, res, next)
+
+  t.equal(next.called, false, 'it does not call next()')
+  t.equal(render.calledWith('decision/failure'), true, 'it calls render() with correct template')
+  resetStubs()
+  t.end()
+})
