f40738c6978a54c0dcf547541b28e2f253ad90a0
Gethyn Jones <gethyn1@gmail.com>
2019-12-12 09:03:34 +0000

    HTBHF-2725 handle duplicate claim decision

--- src/web/routes/application/steps/decision/get-decision-status.js
+++ src/web/routes/application/steps/decision/get-decision-status.js
@@ -1,15 +1,22 @@
-const { equals, compose, prop, cond, always } = require('ramda')
+const { equals, compose, path, prop, cond, always } = require('ramda')
 const { FAIL } = require('./decision-statuses')
+const { DUPLICATE } = require('./eligibility-statuses')
+
+const verificationResultProp = prop => path(['verificationResult', prop])
 
 const outcomeNotMatched = equals('not_matched')
 const outcomeNotConfirmed = equals('not_confirmed')
+const isDuplicate = equals(DUPLICATE)
 
-const identityOutcomeNotMatched = compose(outcomeNotMatched, prop('identityOutcome'))
-const eligibilityOutcomeNotConfirmed = compose(outcomeNotConfirmed, prop('eligibilityOutcome'))
+const identityOutcomeNotMatched = compose(outcomeNotMatched, verificationResultProp('identityOutcome'))
+const eligibilityOutcomeNotConfirmed = compose(outcomeNotConfirmed, verificationResultProp('eligibilityOutcome'))
+
+const isDuplicateClaim = compose(isDuplicate, prop('eligibilityStatus'))
 
 const isFailure = result => identityOutcomeNotMatched(result) || eligibilityOutcomeNotConfirmed(result)
 
 const getDecisionStatus = cond([
+  [isDuplicateClaim, always(FAIL)],
   [isFailure, always(FAIL)]
 ])
 
