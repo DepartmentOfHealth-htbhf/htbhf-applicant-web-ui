2ec94bda2b4f4c63c0ca1481d35c35d8ee269c30
Gethyn Jones <gethyn1@gmail.com>
2019-12-12 16:13:34 +0000

    HTBHF-1943 handle address mismatch in decision status engine

--- src/web/routes/application/steps/decision/get-decision-status.js
+++ src/web/routes/application/steps/decision/get-decision-status.js
@@ -1,23 +1,27 @@
-const { equals, compose, path, prop, cond, always } = require('ramda')
-const { FAIL } = require('./decision-statuses')
+const { equals, compose, path, prop, cond, always, anyPass } = require('ramda')
+const { FAIL, PENDING } = require('./decision-statuses')
 const { DUPLICATE } = require('./eligibility-statuses')
 
 const verificationResultProp = prop => path(['verificationResult', prop])
 
 const outcomeNotMatched = equals('not_matched')
 const outcomeNotConfirmed = equals('not_confirmed')
-const isDuplicate = equals(DUPLICATE)
 
 const identityOutcomeNotMatched = compose(outcomeNotMatched, verificationResultProp('identityOutcome'))
 const eligibilityOutcomeNotConfirmed = compose(outcomeNotConfirmed, verificationResultProp('eligibilityOutcome'))
+const addressLine1MatchNotMatched = compose(outcomeNotMatched, verificationResultProp('addressLine1Match'))
+const postcodeMatchNotMatched = compose(outcomeNotMatched, verificationResultProp('postcodeMatch'))
 
-const isDuplicateClaim = compose(isDuplicate, prop('eligibilityStatus'))
+const isDuplicateClaim = compose(equals(DUPLICATE), prop('eligibilityStatus'))
 
-const isFailure = result => identityOutcomeNotMatched(result) || eligibilityOutcomeNotConfirmed(result)
+const isFailure = anyPass([identityOutcomeNotMatched, eligibilityOutcomeNotConfirmed])
+
+const addressMismatches = anyPass([addressLine1MatchNotMatched, postcodeMatchNotMatched])
 
 const getDecisionStatus = cond([
   [isDuplicateClaim, always(FAIL)],
-  [isFailure, always(FAIL)]
+  [isFailure, always(FAIL)],
+  [addressMismatches, always(PENDING)]
 ])
 
 module.exports = {
