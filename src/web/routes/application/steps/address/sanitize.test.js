98752ed6299ddf4794defe5ae578b325c142ecd4
David Wybourn <david.wybourn@gmail.com>
2019-12-18 13:14:17 +0000

    AFHS-370 Updated postcode regex to match latest claims endpoint (v3/claims)/

--- src/web/routes/application/steps/address/sanitize.test.js
+++ src/web/routes/application/steps/address/sanitize.test.js
@@ -2,19 +2,19 @@ const test = require('tape')
 const sinon = require('sinon')
 const { sanitize } = require('./sanitize')
 
-test('sanitize replaces multiple whitespace with one and saves to a new variable', (t) => {
+test('sanitize replaces multiple whitespace with one, converts to uppercase and saves to a new variable', (t) => {
   const req = {
     body: {
       postcode: 'aa1     1aa'
     }
   }
-  const expectedSanitizedPostcode = 'aa1 1aa'
+  const expectedSanitizedPostcode = 'AA1 1AA'
   const expectedPostcode = 'aa1     1aa'
   const next = sinon.spy()
 
   sanitize()(req, {}, next)
 
-  t.equal(req.body.sanitizedPostcode, expectedSanitizedPostcode, 'it should replace multiple spaces with a single one')
+  t.equal(req.body.sanitizedPostcode, expectedSanitizedPostcode, 'it should replace multiple spaces with a single one and convert to uppercase')
   t.equal(req.body.postcode, expectedPostcode, 'it should not mutate the input')
   t.equal(next.called, true, 'it should called next()')
   t.end()
