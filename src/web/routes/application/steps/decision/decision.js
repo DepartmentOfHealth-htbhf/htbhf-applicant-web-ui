753f72a1f526c97367f5a0fdf96906e775016e00
Mike Smith <mike.smith@sescon.co.uk>
2020-01-10 13:47:58 +0000

    AFHS-588 Add success decision logic to decision.js and add success decision template.

--- src/web/routes/application/steps/decision/decision.js
+++ src/web/routes/application/steps/decision/decision.js
@@ -1,13 +1,17 @@
+const { path } = require('ramda')
 const { configureSessionDetails, handleRequestForPath } = require('../../flow-control')
 const { DECISION_URL, prefixPath } = require('../../paths')
 const { isUndefined } = require('../../../../../common/predicates')
 const { getDecisionStatus } = require('./get-decision-status')
-const { FAIL, PENDING } = require('./decision-statuses')
+const { FAIL, PENDING, SUCCESS } = require('./decision-statuses')
 const { getDecisionPageFallback } = require('./decision-fallback')
+
+const toPounds = pence => (parseInt(pence, 10) / 100).toFixed(2)
 
 const STATUS_TEMPLATE_MAP = {
   [FAIL]: 'failure',
-  [PENDING]: 'pending'
+  [PENDING]: 'pending',
+  [SUCCESS]: 'success'
 }
 
 // TODO: Remove references to "decision fallback" once all stories for epic HTBHF-2014 (receive my decision) are complete.
@@ -16,6 +20,19 @@ const getDecisionPage = (req, res, next) => {
   const { verificationResult, eligibilityStatus } = req.session
   const decisionStatus = getDecisionStatus({ verificationResult, eligibilityStatus })
   const template = STATUS_TEMPLATE_MAP[decisionStatus]
+
+  if (decisionStatus === SUCCESS) {
+    const totalVoucherValueInPence = path(['session', 'voucherEntitlement', 'totalVoucherValueInPence'], req)
+
+    return res.render('decision', {
+      title: req.t('decision.success.title'),
+      body: req.t('decision.success.body', {
+        totalVoucherValue: toPounds(totalVoucherValueInPence),
+        totalVoucherValueForFourWeeks: toPounds(totalVoucherValueInPence * 4)
+      }),
+      template
+    })
+  }
 
   return isUndefined(decisionStatus)
     ? next()
@@ -37,5 +54,6 @@ const registerDecisionRoute = (journey, app) =>
 
 module.exports = {
   registerDecisionRoute,
-  getDecisionPage
+  getDecisionPage,
+  toPounds
 }
