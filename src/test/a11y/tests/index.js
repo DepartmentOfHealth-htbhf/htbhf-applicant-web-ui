08dc3c1c6f199c3cd9332a1d26618f2662ed4667
Jyothi Buddaraju <jyothi.buddaraju@nhs.net>
2020-02-03 11:14:08 +0000

    fix problems while running build

--- src/test/a11y/tests/index.js
+++ src/test/a11y/tests/index.js
@@ -1,10 +1,10 @@
-//const { apply } = require('./apply')
-//const { inScotland } = require('./in-scotland')
-//const { guidance } = require('./guidance')
+// const { apply } = require('./apply')
+// const { inScotland } = require('./in-scotland')
+// const { guidance } = require('./guidance')
 const { configureTestSuite } = require('./configure-test-suite')
 const { nhsNumber } = require('./nhs-number')
 
-//const ALL_TESTS = [apply, inScotland, guidance,nhsNumber]
+// const ALL_TESTS = [apply, inScotland, guidance,nhsNumber]
 
 const ALL_TESTS = [nhsNumber]
 
